e32384d1d45741663089bce9c173bed4173d1899
fdonfi <frederic.fidon@cevennes-parcnational.fr>
2015-04-03 17:22:35 +0200

    front: crud observations fonctionnel

--- web/js/controleurs/baseObservations.js
+++ web/js/controleurs/baseObservations.js
@@ -42,13 +42,13 @@ app.controller('observationSiteListController', function($scope, $routeParams){
     $scope._appName = $routeParams.appName;
 });
 
-app.controller('observationEditController', function($scope, $routeParams, configServ, dataServ){
+app.controller('observationEditController', function($scope, $routeParams, $location, configServ, dataServ){
     $scope._appName = $routeParams.appName;
 
     $scope.setSchema = function(resp){
         $scope.schema = angular.copy(resp);
         if($routeParams.id){
-            dataServ.get($scope._appName + '/observation/' + $routeParams.id, $scope.setData);
+            dataServ.get($scope._appName + '/observation/' + $routeParams.id, $scope.setData, function(resp){}, true);
         }
         else{
             $scope.setData({});
@@ -60,21 +60,62 @@ app.controller('observationEditController', function($scope, $routeParams, confi
         if($scope.data.obsDate){
             $scope.data.obsDate = $scope.data.obsDate.replace(/^(\d+)-(\d+)-(\d+).*$/i, "$3/$2/$1");
         }
+        angular.forEach($scope.schema.formObs, function(value){
+            if(value.name!='observateurs'){
+                if($scope.data[value.name] == undefined){
+                    $scope.data[value.name] = null;
+                }
+            }
+        }, $scope);
         $scope.obrs = [];
         if($scope.data.observateurs){
             angular.forEach($scope.data.observateurs, function(obr){
                 $scope.obrs.push(obr.obrId);
             }, $scope);
         }
-        $scope.data.observateurs = $scope.obrs;
-
+        $scope.data.observateurs = angular.copy($scope.obrs);
+        dataServ.get($scope._appName + '/site/' + $scope.data.siteId, $scope.setSite);
     };
+
+    $scope.setSite = function(resp){
+        $scope.site = resp;
+    };
+
+
+    $scope.save = function(){
+        if($routeParams.id){
+            dataServ.post($scope._appName + '/observation/' + $routeParams.id, $scope.data, $scope.saved, $scope.unsaved);
+        }
+        else{
+            dataServ.put($scope._appName + '/observation', $scope.data, $scope.saved, $scope.unsaved);
+        }
+    };
+
+    $scope.saved = function(resp){
+        dataServ.forceReload = true;
+        $location.path($scope._appName + '/observation/' + resp.id);
+    };
+
+    $scope.unsaved = function(resp){
+        $scope.errors = resp;
+    };
+
+    $scope.remove = function(){
+        if(confirm('Effacement observation')){
+            dataServ.delete($scope._appName + '/observation/' + $routeParams.id, $scope.removed);
+        }
+    };
+
+    $scope.removed = function(resp){
+        dataServ.forceReload = true;
+        $location.path($scope._appName + '/site/' + $scope.data.siteId);
+    }
 
     configServ.getUrl($scope._appName + '/obsConfig', $scope.setSchema);
 });
 
 
-app.controller('observationSiteEditController', function($scope, $routeParams, configServ){
+app.controller('observationSiteEditController', function($scope, $routeParams, $location, configServ, dataServ){
     $scope._appName = $routeParams.appName;
 
     $scope.setSchema = function(resp){
@@ -84,12 +125,26 @@ app.controller('observationSiteEditController', function($scope, $routeParams, c
 
     $scope.setData = function(resp){
         $scope.data = angular.copy(resp);
+        angular.forEach($scope.schema.formObs, function(value){
+            $scope.data[value.name] = value.default || null;
+        }, $scope);
+        $scope.data.siteId = $routeParams.id;
         $scope.data.observateurs = [null];
+        dataServ.get($scope._appName + '/site/' + $routeParams.id, $scope.setSite);
+    };
 
+    $scope.setSite = function(resp){
+        $scope.site = resp;
     };
 
     $scope.save = function(){
+        dataServ.put($scope._appName + '/observation', $scope.data, function(resp){
+            dataServ.forceReload = true;
+            $location.path($scope._appName + '/observation/' + resp.id);
 
+        }, function(resp, status){
+            $scope.errors = resp;
+        });
     };
 
     $scope.remove = function(){
