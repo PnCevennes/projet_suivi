48c7fd7c06ba4b1ee372448ec4cb9805b1ee18fb
fdonfi <frederic.fidon@cevennes-parcnational.fr>
2015-04-16 12:42:02 +0200

    adaptation formulaire taxon nouvelle config

--- web/js/controleurs/baseTaxons.js
+++ web/js/controleurs/baseTaxons.js
@@ -57,65 +57,17 @@ app.controller('taxonDetailController', function($scope, $routeParams, configSer
 
 app.controller('taxonEditController', function($scope, $routeParams, $location, configServ, dataServ){
     $scope._appName = $routeParams.appName;
-    $scope.errors = [];
-    $scope.setSchema = function(resp){
-        $scope.schema = angular.copy(resp.formObsTx);
-        if($routeParams.id){
-            dataServ.get($scope._appName + '/obs_taxon/' + $routeParams.id, $scope.setData);
-        }
-        else{
-            $scope.tmp = {obsId: $routeParams.obs_id};
-            angular.forEach($scope.schema, function(item){
-                $scope.tmp[item.name] = item.default || null;
-            }, $scope);
-            $scope.setData($scope.tmp);
-        }
-    };
-
-    $scope.setData = function(resp){
-        $scope.data = angular.copy(resp);
-
-    };
-
-    $scope.save = function(){
-        if($routeParams.id){
-            dataServ.post(
-                $scope._appName + '/obs_taxon/' + $routeParams.id, 
-                $scope.data, 
-                function(resp){
-                    dataServ.forceReload = true;
-                    $location.path($scope._appName + '/taxons/' + resp.id);
-                }, 
-                function(resp){
-                    $scope.errors = resp;
-                });
-        }
-        else{
-            dataServ.put(
-                $scope._appName + '/obs_taxon', 
-                $scope.data, 
-                function(resp){
-                    dataServ.forceReload = true;
-                    $location.path($scope._appName + '/taxons/' + resp.id);
-                }, 
-                function(resp){
-                    $scope.errors = resp;
-                });
-        }
-    };
-
-    $scope.removed = function(resp){
-        dataServ.forceReload = true;
-        $location.path($scope._appName + '/observation/' + $scope.data.obsId);
+    
+    $scope.configUrl = $scope._appName + '/config/obstaxon/form';
+    if($routeParams.id){
+        $scope.saveUrl = $scope._appName + '/obs_taxon/' + $routeParams.id;
+        $scope.dataUrl = $scope._appName + '/obs_taxon/' + $routeParams.id;
+        $scope.data = {};
     }
-
-    $scope.remove = function(){
-        if(confirm('Effacement obs. taxon')){
-            dataServ.delete($scope._appName + '/obs_taxon/' + $routeParams.id, $scope.removed);
-        }
-    };
-
-    configServ.getUrl($scope._appName + '/obsTxConfig', $scope.setSchema);
+    else{
+        $scope.saveUrl = $scope._appName + '/obs_taxon';
+        $scope.data = {obsId: $routeParams.obs_id};
+    }
 });
 
 app.controller('taxonListController', function(){
