d0d5912d258c940510c7c2e39df61908a1434539
fdonfi <frederic.fidon@cevennes-parcnational.fr>
2015-05-12 18:27:59 +0200

    fix erreur de page lorsqu'un site sélectionné sur la carte est à un index multiple du nombre d'éléments affichés

--- web/js/controleurs/baseSites.js
+++ web/js/controleurs/baseSites.js
@@ -159,7 +159,8 @@ app.controller('siteListController', function($scope, $rootScope, $routeParams, 
             var cnt = data.length;
             var res = $filter('filter')(data, {id: item.feature.properties.id});
             var idx = data.indexOf(res[0]);
-            var pg = idx / $scope.tableParams.count();
+            var pg = (idx+1) / $scope.tableParams.count();
+            console.log(pg);
             $scope.tableParams.page(Math.ceil(pg) || 1);
 
         });
