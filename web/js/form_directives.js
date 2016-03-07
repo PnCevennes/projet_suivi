0139898f173c5bf1335daa06488e8f89cdb5c23b
fdonfi <frederic.fidon@cevennes-parcnational.fr>
2016-03-07 15:23:08 +0100

    front : ajout directive subform

--- web/js/form_directives.js
+++ web/js/form_directives.js
@@ -73,6 +73,7 @@ app.directive('dynform', function(){
     return {
         restrict: 'E',
         scope: {
+            name: '@',
             group: '=',
             data: '=',
             errors: '=',
@@ -92,6 +93,36 @@ app.directive('tableFieldset', function(){
         },
         templateUrl: 'js/templates/form/tableFieldset.htm',
         controller: function($scope){},
+    };
+});
+
+app.directive('subform', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            refer: '=',
+            schema: '=',
+        },
+        templateUrl: 'js/templates/form/subform.htm',
+        controller: function($scope){
+            if($scope.refer == undefined){
+                $scope.refer = [{}];
+            }
+            
+            $scope.$watch(function(){return $scope.refer}, function(nv, ov){
+                if(nv !== ov){
+                    $scope.refer = nv; 
+                }
+            });
+            
+            $scope.add_item = function(){
+                $scope.refer.push({});
+            };
+
+            $scope.remove_item = function(idx){
+                $scope.refer.splice(idx, 1);
+            };
+        }
     };
 });
 
