552f20b273c3571dc7d2c3c6c4b44b23afa35592
fdonfi <frederic.fidon@cevennes-parcnational.fr>
2016-05-11 12:22:14 +0200

    modif breadcrumb

--- src/PNC/ChiroBundle/Controller/ConfigController.php
+++ src/PNC/ChiroBundle/Controller/ConfigController.php
@@ -7,91 +7,103 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+use Commons\Exceptions\DataObjectException;
+
 
 class ConfigController extends Controller{
+
+    public function getBcData($req, $field, $val){
+
+        $manager = $this->getDoctrine()->getConnection();
+
+        $req = $manager->prepare($req);
+        $req->bindValue($field, $val);
+        $req->execute();
+        $res = $req->fetchAll();
+        if(!isset($res[0])) throw new DataObjectException();
+        return array('label'=>$res[0]['label'], 'next'=>$res[0]['next']);
+    }
+
     //path : chiro/breadcrumb
     public function breadcrumbAction(Request $req){
         $view = $req->get('view');
         $id = $req->get('id');
         $generic = $req->get('generic') == "true";
-
-        $manager = $this->getDoctrine()->getConnection();
-
         $out = array();
-        switch($view){
-            case 'biometrie':
-                $req = $manager->prepare('SELECT id, fk_cotx_id FROM chiro.subpr_observationtaxon_biometrie WHERE id=:id');
-                $req->bindValue('id', $id);
-                $req->execute();
-                $res = $req->fetchAll();
-                if(!isset($res[0])){
-                    return new JsonResponse(array('id'=>null), 404);
-                }
-                $res = $res[0];
-                $out[] = array(
-                    'id'=>$res['id'], 
-                    'label'=>'Biometrie n°'.$id, 
-                    'link'=> $generic ? '#/g/chiro/biometrie/detail/'.$id : '#/chiro/biometrie/'.$id
-                );
-                $id = $res['fk_cotx_id'];
-            case 'taxons': 
-            case 'taxon':
-                $req = $manager->prepare('SELECT id, cotx_nom_complet as label, fk_bv_id FROM chiro.pr_visite_observationtaxon WHERE id=:id');
-                $req->bindValue('id', $id);
-                $req->execute();
-                $res = $req->fetchAll();
-                if(!isset($res[0])){
-                    return new JsonResponse(array('id'=>null), 404);
-                }
-                $res = $res[0];
-                $out[] = array(
-                    'id'=>$res['id'], 
-                    'label'=>$res['label'], 
-                    'link'=> $generic ? '#/g/chiro/taxons/detail/'.$id : '#/chiro/taxons/'.$id
-                );
-                $id = $res['fk_bv_id'];
-            case 'validation':
-                if($view == 'validation'){
-                    return new JsonResponse(array(array('id'=>null, 'label'=>'Validation', 'link'=>'#/chiro/validation')));
-                }
-            case 'inventaire':
-            case 'observation':
-                $req = $manager->prepare('SELECT id, bv_date as label, fk_bs_id FROM suivi.pr_base_visite WHERE id=:id');
-                $req->bindValue('id', $id);
-                $req->execute();
-                $res = $req->fetchAll();
-                if(!isset($res[0])){
-                    return new JsonResponse(array(array('id'=>null, 'label'=>'Inventaire', 'link'=>'#/chiro/inventaire')));
-                }
-                $res = $res[0];
-                if($res['fk_bs_id']==null){
+        try{
+            switch($view){
+                case 'biometrie':
+                    $data = $this->getBcData(
+                        'SELECT id as label, fk_cotx_id as next FROM chiro.subpr_observationtaxon_biometrie WHERE id=:id',
+                        'id',
+                        $id);
                     $out[] = array(
-                        'id'=>$res['id'], 
-                        'label'=>implode('/', array_reverse(explode('-', $res['label']))), //transformation date Y-m-d -> d/m/Y
-                        'link'=> $generic ? '#/g/chiro/inventaire/detail/'.$id : '#/chiro/inventaire/'.$id);
+                        'id'=>$id, 
+                        'label'=>'Biometrie n°'.$data['label'], 
+                        'link'=> $generic ? '#/g/chiro/biometrie/detail/'.$id : '#/chiro/biometrie/'.$id
+                    );
+                    $id = $data['next'];
+                case 'taxons':
+                case 'taxon':
+                    $data = $this->getBcData(
+                        'SELECT id, cotx_nom_complet as label, fk_bv_id as next FROM chiro.pr_visite_observationtaxon WHERE id=:id',
+                        'id',
+                        $id);
                     $out[] = array(
-                        'id'=>null, 
-                        'label'=>'Inventaire', 
-                        'link'=> $generic ? '#/g/chiro/inventaire/list' : '#/chiro/inventaire');
-                    return new JsonResponse(array_reverse($out));
-                }
-                $out[] = array(
-                    'id'=>$res['id'], 
-                    'label'=>implode('/', array_reverse(explode('-', $res['label']))), //transformation date Y-m-d -> d/m/Y
-                    'link'=> $generic ? '#/g/chiro/observation/detail/'.$id : '#/chiro/observation/'.$id);
-                $id = $res['fk_bs_id'];
-            case 'site':
-                $req = $manager->prepare('SELECT id, bs_nom as label FROM suivi.pr_base_site WHERE id=:id');
-                $req->bindValue('id', $id);
-                $req->execute();
-                $res = $req->fetchAll();
-                if(!isset($res[0])){
-                    return new JsonResponse(array(array('id'=>null, 'label'=>'Sites', 'link'=>'#/chiro/site')));
-                }
-                $res = $res[0];
-                $out[] = array('id'=>$res['id'], 'label'=>$res['label'], 'link'=>$generic ? '#/g/chiro/site/detail/'.$id : '#/chiro/site/'.$id);
-                $out[] = array('id'=>null, 'label'=>'Sites', 'link'=>$generic ? '#/g/chiro/site/list' : '#/chiro/site');
+                        'id'=>$id, 
+                        'label'=>$data['label'], 
+                        'link'=> $generic ? '#/g/chiro/taxons/detail/'.$id : '#/chiro/taxons/'.$id
+                    );
+                    $id = $data['next'];
+                case 'validation':
+                    if($view == 'validation'){
+                        return new JsonResponse(array(array('id'=>null, 'label'=>'Validation', 'link'=>'#/chiro/validation')));
+                    }
+                case 'inventaire':
+                case 'observation':
+                    if(!isset($id)){
+                        $out[] = array(
+                            'id'=>null, 
+                            'label'=>'Inventaire', 
+                            'link'=> $generic ? '#/g/chiro/inventaire/list' : '#/chiro/inventaire');
+                        return new JsonResponse(array_reverse($out));
+                    }
+                    $data = $this->getBcData(
+                        'SELECT id, bv_date as label, fk_bs_id as next FROM suivi.pr_base_visite WHERE id=:id',
+                        'id',
+                        $id);
+                    if($data['next']==null){
+                        $out[] = array(
+                            'id'=>$id, 
+                            'label'=>implode('/', array_reverse(explode('-', $data['label']))), //transformation date Y-m-d -> d/m/Y
+                            'link'=> $generic ? '#/g/chiro/inventaire/detail/'.$id : '#/chiro/inventaire/'.$id);
+                        $out[] = array(
+                            'id'=>null, 
+                            'label'=>'Inventaire', 
+                            'link'=> $generic ? '#/g/chiro/inventaire/list' : '#/chiro/inventaire');
+                        return new JsonResponse(array_reverse($out));
+                    }
+                    $out[] = array(
+                        'id'=>$id, 
+                        'label'=>implode('/', array_reverse(explode('-', $data['label']))), //transformation date Y-m-d -> d/m/Y
+                        'link'=> $generic ? '#/g/chiro/observation/detail/'.$id : '#/chiro/observation/'.$id);
+                    $id = $data['next'];
+                case 'site':
+                    if(isset($id)){
+                        $data = $this->getBcData(
+                            'SELECT id, bs_nom as label, null as next FROM suivi.pr_base_site WHERE id=:id',
+                            'id',
+                            $id);
+                        $out[] = array('id'=>$id, 'label'=>$data['label'], 'link'=>$generic ? '#/g/chiro/site/detail/'.$id : '#/chiro/site/'.$id);
+                    }
+                    $out[] = array('id'=>null, 'label'=>'Sites', 'link'=>$generic ? '#/g/chiro/site/list' : '#/chiro/site');
+            }
+            return new JsonResponse(array_reverse($out));
+
         }
-        return new JsonResponse(array_reverse($out));
+        catch(DataObjectException $e){
+            return new JsonResponse(array('id'=>null), 404);
+        }
+
     }
 }
